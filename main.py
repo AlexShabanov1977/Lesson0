ef186afdad376b3910e128a38e677331e4cf1ca9
AlexShabanov1977 <avsh77@mail.ru>
2024-06-09 13:36:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+print("hello world")
+print("Задача - Длина слова")
+a="мне инетерсно изучить язык программирования для решения текузцей задачи"
+print(len(a))
+print("Задача - Суммы разности")
+first=5
+second=127
+summa=first+second
+diff=first-second
+print("Сумма",summa)
+print("Разницв",diff)
+print("Задание - Среднее арифметическое")
+third=147
+mean=(first+second+third)/3
+print("Среднее арифметическое = ",mean)
+print("Задание- Простые строчки")
+first_string="Понедельник"
+second_string="Вторник"
+print(first_string+", "+ second_string)
+print("Задача - Сложная формула")
+a=2
+b=2
+c=2
+f=(a*b)+(a*c)
+print("Решение =",f**2/2)
+
+
